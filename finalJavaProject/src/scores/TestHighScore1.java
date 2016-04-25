157c0b5c69f34ef8d6ab2096ee130133040d0d87
Mehdqtx <mehdi.delvaux@gmail.com>
2016-04-25 18:15:57 +0200

    Part 1

--- finalJavaProject/src/scores/TestHighScore1.java
+++ finalJavaProject/src/scores/TestHighScore1.java
@@ -1,10 +1,44 @@
 package scores;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class TestHighScore1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entrez votre prenom :");
+		String str = sc.nextLine();
+		System.out.println(" Bienvenue " + str);
+		List<String> values = new ArrayList<String>(); 
+		
+		
+		try (BufferedReader br = new BufferedReader(new FileReader("scoreSamples.txt")))
+		{
+			String sCurrentLine;
 
+			while ((sCurrentLine = br.readLine()) != null) {
+				values.add(sCurrentLine);
+			}
+			
+			Random rnd = new Random();
+			int rndValue = rnd.nextInt(values.size());
+
+			values.get(rndValue);
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
-
 }
