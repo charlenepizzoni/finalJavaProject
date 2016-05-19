4831b35f1b918b9a978ddf01c1acbdf491b711a0
mehdi delvaux <mehdi.delvaux@gmail.com>
2016-05-19 19:03:26 +0200

    Step 2
    Ajout de l'affichage des 10 best score dans TestHighScore2
    
    getScore permet d'obtenir tout les score (passage en List)

--- finalJavaProject/src/scores/HighScore2.java
+++ finalJavaProject/src/scores/HighScore2.java
@@ -5,18 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * manages high scores.
  */
-public class HighScore {
+public class HighScore2 {
 
+	
+
+	
 	/** 
 	 * retrieves High Scores from web server 
 	 * @return an array containing high scores
 	 */
-	public String[] getScores() {
-		String scores[] = new String[10];
+	public List<String> getScores() {
+		
+		List<String> scores = new ArrayList<>();
+		//String scores[] = new String[10];
 		
 	    URL url;
 		try {
@@ -24,9 +31,9 @@ public class HighScore {
 	        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 	        String inputLine;
 	        int i = -1;
-	        while (((inputLine = in.readLine()) != null) && (i < 10)) {
+	        while (((inputLine = in.readLine()) != null)) {
 	        	if (i >= 0) { // we don't want the first line
-	        		scores[i] = inputLine;
+	        		scores.add(inputLine);	
 	        	}
 	        	i++;
 	        }
@@ -39,6 +46,28 @@ public class HighScore {
 	
 		return scores;
 	}
+	
+	/**
+	*
+	*/
+
+	public BestPlayer[] tenBestScores(List<String> readScores) {
+		
+		
+		
+		BestPlayer[] allBest = new BestPlayer[10];
+		int nb = 0;
+		for (String s : readScores){
+			String[] parse = s.split(",");
+			if (s.length() == 4){
+				BestPlayer bp = new BestPlayer(parse[2], Integer.parseInt(parse[3]));
+				nb ++;
+				allBest[nb-1] = bp;
+			} // on ne prend pas les lignes ne contenant pas de score.
+		}
+		return allBest;
+	}
+	
 	
 
 }
